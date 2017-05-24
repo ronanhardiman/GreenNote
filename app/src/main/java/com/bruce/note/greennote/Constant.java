4c971e95b09e0f5b2fe476686092e0dc4315926d
bruce <1737036540@qq.com>
2017-05-25 00:06:19 +0800

    add permission ...

---
+++ app/src/main/java/com/bruce/note/greennote/Constant.java
@@ -0,0 +1,8 @@
+package com.bruce.note.greennote;
+
+/**
+ * Created by liq on 2017/5/24.
+ */
+
+public class Constant {
+}
---
+++ app/src/main/java/com/bruce/note/greennote/model/preference/PreferenceHelper.java
@@ -0,0 +1,9 @@
+package com.bruce.note.greennote.model.preference;
+
+/**
+ * Created by liq on 2017/5/25.
+ */
+
+public class PreferenceHelper {
+
+}
