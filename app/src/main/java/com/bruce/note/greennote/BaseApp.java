3774d24195fe7c712b90042e0d1b92e10ef0342d
bruce <1737036540@qq.com>
2017-05-24 23:21:09 +0800

    add base

---
+++ app/src/main/java/com/bruce/note/greennote/BaseApp.java
@@ -0,0 +1,15 @@
+package com.bruce.note.greennote;
+
+import android.app.Application;
+
+/**
+ * Created by liq on 2017/5/23.
+ */
+
+public class BaseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/bruce/note/greennote/activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.bruce.note.greennote.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liq on 2017/5/24.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
