bfa479460f27676fe4d46380c8d4c134971d11ff
bruce <1737036540@qq.com>
2017-05-25 08:13:27 +0800

    add fragmentation

---
+++ app/src/main/java/com/bruce/note/greennote/view/fragment/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.bruce.note.greennote.view.fragment;
+
+import me.yokeyword.fragmentation.SupportFragment;
+
+/**
+ * Created by liq on 2017/5/25.
+ */
+
+public class BaseFragment extends SupportFragment{
+}
